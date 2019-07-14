b9af52570d0b3a0bac8a0943f02cad03585a1211
axb2035 <contact@blendertraining.com>
2019-07-15 08:02:37 +1000

    Iniital load
    Initial load of all 'play' files.

---
+++ chase_seq_png_generator.py
@@ -0,0 +1,99 @@
+import pandas as pd
+
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+
+# PC font path
+font_path = 'C:/Windows/Fonts/'
+courier = 'courbd.ttf'
+
+# Mac font path
+# font_path = 'Library/Fonts/'
+# courier = 'cour.dfont'
+
+fnt_a_40 = ImageFont.truetype(font_path + 'Arial.ttf', 40)
+fnt_a_100 = ImageFont.truetype(font_path + 'Arial.ttf', 100)
+fnt_c_40 = ImageFont.truetype(font_path + courier, 40)
+
+
+def write_png_sequence(chase_seq):
+    for row in range(chase_seq.shape[0]):
+        arena = chase_seq.iloc[row, 5:406].values.reshape(20, 20)
+        e = str(chase_seq.iloc[row, 0])
+        s = str(chase_seq.iloc[row, 1])
+        a = str(chase_seq.iloc[row, 2])
+        r = str(chase_seq.iloc[row, 3])
+        arena = np.array2string(arena)
+        
+        arena = ' ' + arena.replace('0',' ').replace('1','X').replace('2','X').replace('3','R').replace('4','A').replace('[','').replace(']','')
+        
+        img = Image.new('RGB', (1920, 1080), color = 'white')
+        d = ImageDraw.Draw(img)
+        
+        d.text((24, 10), 'CHASE', font=fnt_a_100, fill=(0,0,0))
+        d.text((32, 108), 'A toy-text reinforcement learning environment', font=fnt_a_40, fill=(0,0,0))    
+        d.text((10,170), arena, font=fnt_c_40, fill=(0,0,0))
+        d.text((32, 940), 'Episode: ' + e 
+                          + '        Step: ' + s
+                          + '        Action: ' + a
+                          + '        Reward: ' + r, font=fnt_a_40, fill=(0,0,0))    
+        
+        if row%10 == 0:
+            print('Completed frame:', row)
+    
+        img.save('frames/test' + str(row).zfill(4) + '.png')    
+
+
+# ------
+try:
+    chase_seq = pd.read_csv('Chase - exp_23e_train - 20190714 - 1239.csv')
+except:
+    print('Can not open log file.')
+
+#chase_seq = chase_seq[chase_seq['Episode'] >= 290000]
+#chase_seq.tail()
+# write_png_sequence(chase_seq)
+
+# SOME DESCRIPTIVE STATS...
+chase_seq['Reward'].replace('None', '0', inplace=True)
+chase_seq['Reward'] = chase_seq['Reward'].astype('int64')
+#chase_seq['Reward'].dtype
+
+# Total episodes.
+chase_ep = chase_seq['Episode'].value_counts().count()
+print("Total episodes:", chase_ep)
+
+# Least number of steps.
+chase_min_steps = chase_seq.groupby('Episode')['Step'].max().min()
+print("Min steps:", chase_min_steps)
+
+# Mean number of steps.
+chase_mean_steps = chase_seq.groupby('Episode')['Step'].max().mean()
+print("Mean steps:", chase_mean_steps)
+
+# Most number of steps.
+chase_max_steps = chase_seq.groupby('Episode')['Step'].max().max()
+print("Max steps:", chase_max_steps)
+
+# Mean of reward.
+chase_mean_reward = chase_seq['Reward'].sum()/chase_ep
+print("Mean reward:", round(chase_mean_reward, 4), ' (Random agent: -0.4257)')
+
+# Breakdown of scores
+reward_breakdown = chase_seq.groupby('Episode')['Reward'].sum().value_counts()
+reward_breakdown.sort_index(inplace=True)
+print('Breakdown of reward\n', reward_breakdown)
+
+# Games won
+try:
+    pct_games_won = (reward_breakdown[5]/chase_ep)*100
+except:
+    pct_games_won = 0
+    
+print('Games won : ', round(pct_games_won, 2), '% (Random agent: 0.08%)')
+
+
+max_series = chase_seq[chase_seq['Step'] == 1001]
+max_episodes = max_series['Episode'].values
+max_seq = chase_seq[(chase_seq['Episode'].isin(max_episodes)) & (chase_seq['Step'] < 100)]
+write_png_sequence(max_seq)
---
+++ gym-chase simple agents.py
@@ -0,0 +1,152 @@
+"""
+Created on Mon Jun 24 16:29:35 2019
+
+Create a basic test bed for the Chase gym environment...
+"""
+import numpy as np
+import copy
+
+from datetime import datetime
+
+def write_chase_log(log, agent_name):
+    file_time = datetime.now().strftime("%Y%m%d - %H%M")
+    f_name = 'Chase - ' + agent_name + ' - ' + file_time + '.csv'
+    with open(f_name, "w", newline="") as f:
+        h_line = 'Episode,Step,Action,Reward,Done,{}\n'.format(np.array2string(np.arange(0.0, 400.0)).replace('.', ',').replace('\n', '').replace(' ', '')[1:-2])
+        f.write(h_line)
+        for i in range(len(log)):
+            l_line = '{},{},{},{},{},{}\n'.format(log[i][0],
+                                                  log[i][1],
+                                                  log[i][2],
+                                                  log[i][3],
+                                                  log[i][4],
+                                                  str(log[i][5]).replace('.', ',').replace('\n', '')[1:-2]) 
+            f.write(l_line)
+        f.close
+       
+
+import gym
+#from gym_chase.envs import ChaseEnv
+#env = gym.make('gym_chase:Chase-v0')
+
+EPISODES = 10000
+e = 0
+state_log = []
+
+"""
+# Simple human agent
+
+while e < EPISODES:
+    done = False
+    e_step = 0
+    total_reward = 0
+    state = env.reset(random_seed=e)
+    state = state.ravel()
+    
+    state_log.append([e, e_step, None, None, done, copy.deepcopy(state)])
+    
+    while not done:
+        env.render()
+        print('\n7   8   9')
+        print('  \\ | /')
+        print('4 - 5 - 6')
+        print('  / | \\')
+        print('1   2   3')
+        p_move = input('\nYour move [1-9 move, 5 stay still]:')
+        n_state, r, done = env.step(int(p_move))
+        print('\nEpisode:', e, 'Step:', e_step)
+        print('\nReward:', r)
+        total_reward += r
+        e_step += 1
+        n_state = n_state.ravel()
+        state_log.append([e, e_step, p_move, r, done, copy.deepcopy(n_state)])
+    env.render()
+    if total_reward == 5:
+        print("\nAll robots eliminated. Total reward =", total_reward)
+    else:
+        print("\nAgent eliminated. Total reward =", total_reward)        
+    e += 1
+
+
+write_chase_log(state_log, 'Human')
+
+# Simple random agent
+
+import random
+from random import randrange
+
+while e < EPISODES:
+    done = False
+    e_step = 0
+    total_reward = 0
+    state = env.reset(random_seed=e)
+    state = state.ravel()
+    state_log.append([e, e_step, None, None, done, copy.deepcopy(state)])
+    
+    random.seed()
+    while not done:
+        # time.sleep(2)
+        env.render()
+        rnd_move = randrange(9) + 1
+        n_state, r, done = env.step(rnd_move)
+        print('\nReward:', r)
+        total_reward += r 
+        e_step += 1
+        n_state = n_state.ravel()
+        state_log.append([e, e_step, rnd_move, r, done, copy.deepcopy(n_state)])
+    env.render()
+#    if total_reward == 5:
+#        print("\nAll robots eliminated. Total reward =", total_reward)
+#    else:
+#        print("\nAgent eliminated. Total reward =", total_reward)
+    e += 1
+
+write_chase_log(state_log, 'Random')
+
+""" 
+# Evaluate keras agent
+
+from keras.models import load_model
+
+model_name = 'exp_23g_train'
+
+try:
+    dqn_model = load_model(str(model_name) + '.h5')
+except:
+    print('Could not open model file...')
+
+while e < EPISODES:
+    done = False
+    e_step = 0
+    total_reward = 0
+    state = env.reset(random_seed=e)
+    state = state.ravel()
+    state_log.append([e, e_step, None, None, done, copy.deepcopy(state)])
+
+    while not done:
+        #cat_state = to_categorical(state, num_classes=5)
+        act_values = dqn_model.predict(state.reshape(1, -1)/4)
+        act = np.argmax(act_values[0]) + 1
+        
+        n_state, r, done = env.step(act)
+        #env.render()
+        #print('\nAction:', act, 'Reward:', r)
+        total_reward += r 
+        e_step += 1
+        n_state = n_state.ravel()
+        state_log.append([e, e_step, rnd_move, r, done, copy.deepcopy(n_state)])
+        state = n_state
+        if e_step > 1000:
+            print('Steps greater than 1000 - breaking', e)
+            done = True
+    #env.render()
+#    if total_reward == 5:
+#        print("\nAll robots eliminated. Total reward =", total_reward)
+#    else:
+#        print("\nAgent eliminated. Total reward =", total_reward)
+    if e % 100 == 0:
+        print('Completed episode', e)
+    e += 1
+    
+write_chase_log(state_log, str(model_name) + "_val")
+
