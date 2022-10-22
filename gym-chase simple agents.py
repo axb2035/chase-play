6d319a810840b585583abdd6f0b829ce01e07986
axb2035 <axb2035@gmail.com>
2022-10-23 09:03:11 +1030

    Remove keras agent

--- gym-chase simple agents.py
+++ gym-chase simple agents.py
@@ -89,8 +89,7 @@ while e < EPISODES:
     while not done:
         # time.sleep(2)
         # env.render()
-        rnd_move = randrange(9) + 1
-        # rnd_move = 5
+        rnd_move = randrange(9) + 1        
         n_state, r, done, dummy, info = env.step(rnd_move)
         # print('\nReward:', r)
         total_reward += r 
@@ -105,50 +104,3 @@ while e < EPISODES:
     e += 1
 
 # write_chase_log(state_log, 'Random')
-
-# Evaluate keras agent
-"""
-from keras.models import load_model
-
-model_name = 'exp_23g_train'
-
-try:
-    dqn_model = load_model(str(model_name) + '.h5')
-except:
-    print('Could not open model file...')
-
-while e < EPISODES:
-    done = False
-    e_step = 0
-    total_reward = 0
-    state = env.reset(random_seed=e)
-    state = state.ravel()
-    state_log.append([e, e_step, None, None, done, copy.deepcopy(state)])
-
-    while not done:
-        #cat_state = to_categorical(state, num_classes=5)
-        act_values = dqn_model.predict(state.reshape(1, -1)/4)
-        act = np.argmax(act_values[0]) + 1
-        
-        n_state, r, done = env.step(act)
-        #env.render()
-        #print('\nAction:', act, 'Reward:', r)
-        total_reward += r 
-        e_step += 1
-        n_state = n_state.ravel()
-        state_log.append([e, e_step, rnd_move, r, done, copy.deepcopy(n_state)])
-        state = n_state
-        if e_step > 1000:
-            print('Steps greater than 1000 - breaking', e)
-            done = True
-    #env.render()
-#    if total_reward == 5:
-#        print("\nAll robots eliminated. Total reward =", total_reward)
-#    else:
-#        print("\nAgent eliminated. Total reward =", total_reward)
-    if e % 100 == 0:
-        print('Completed episode', e)
-    e += 1
-    
-write_chase_log(state_log, str(model_name) + "_val")
-"""
