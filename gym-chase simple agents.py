8518f11c800bf0ec455586c621310e471fa54e44
axb2035 <axb2035@gmail.com>
2022-10-23 11:48:15 +1030

    Update for flake8 linting and Black formatting. Improve log writer.
    As part of updating to flake8/black long lines needed to be reduced. To do so implemented a way to do the log writing which removed the need for numpy.

--- gym-chase simple agents.py
+++ gym-chase simple agents.py
@@ -3,34 +3,39 @@ Created on Mon Jun 24 16:29:35 2019
 
 Create a basic test bed for the Chase gym environment...
 """
-import numpy as np
-import copy
+import gym
+from copy import deepcopy
 
 from datetime import datetime
+import random
+from random import randrange
+
 
 def write_chase_log(log, agent_name):
     file_time = datetime.now().strftime("%Y%m%d - %H%M")
     f_name = 'Chase - ' + agent_name + ' - ' + file_time + '.csv'
     with open(f_name, "w", newline="") as f:
-        h_line = 'Episode,Step,Action,Reward,Done,{}\n'.format(np.array2string(np.arange(0.0, 400.0)).replace('.', ',').replace('\n', '').replace(' ', '')[1:-2])
+        arena_pos = ""
+        for v in range(400):
+            arena_pos += str(v) + ","
+        h_line = 'Episode,Step,Action,Reward,Done,' + arena_pos + '\n'
         f.write(h_line)
         for i in range(len(log)):
+            log[i][5] = str(log[i][5]).replace('.', ',')
+            log[i][5] = log[i][5].replace('\n', '')[1:-2]
             l_line = '{},{},{},{},{},{}\n'.format(log[i][0],
                                                   log[i][1],
                                                   log[i][2],
                                                   log[i][3],
                                                   log[i][4],
-                                                  str(log[i][5]).replace('.', ',').replace('\n', '')[1:-2]) 
+                                                  log[i][5],)
             f.write(l_line)
         f.close
-       
 
-import gym
-import gym_chase
 
 env = gym.make('gym_chase:Chase-v0')
 
-EPISODES = 10000
+EPISODES = 10
 e = 0
 state_log = []
 
@@ -43,9 +48,9 @@ while e < EPISODES:
     total_reward = 0
     state = env.reset(random_seed=e)
     state = state.ravel()
-    
-    state_log.append([e, e_step, None, None, done, copy.deepcopy(state)])
-    
+
+    state_log.append([e, e_step, None, None, done, deepcopy(state)])
+
     while not done:
         env.render()
         print('\n7   8   9')
@@ -60,12 +65,12 @@ while e < EPISODES:
         total_reward += r
         e_step += 1
         n_state = n_state.ravel()
-        state_log.append([e, e_step, p_move, r, done, copy.deepcopy(n_state)])
+        state_log.append([e, e_step, p_move, r, done, deepcopy(n_state)])
     env.render()
     if total_reward == 5:
         print("\nAll robots eliminated. Total reward =", total_reward)
     else:
-        print("\nAgent eliminated. Total reward =", total_reward)        
+        print("\nAgent eliminated. Total reward =", total_reward)
     e += 1
 
 
@@ -74,28 +79,25 @@ write_chase_log(state_log, 'Human')
 """
 # Simple random agent
 
-import random
-from random import randrange
-
 while e < EPISODES:
     done = False
     e_step = 0
     total_reward = 0
-    state = env.reset(random_seed=e)
+    state = env.reset(seed=e)
     state = state.ravel()
-    state_log.append([e, e_step, None, None, done, copy.deepcopy(state)])
-    
+    state_log.append([e, e_step, None, None, done, deepcopy(state)])
+
     random.seed()
     while not done:
         # time.sleep(2)
         # env.render()
-        rnd_move = randrange(9) + 1        
+        rnd_move = randrange(9) + 1
         n_state, r, done, dummy, info = env.step(rnd_move)
         # print('\nReward:', r)
-        total_reward += r 
+        total_reward += r
         e_step += 1
         n_state = n_state.ravel()
-        state_log.append([e, e_step, rnd_move, r, done, copy.deepcopy(n_state)])
+        state_log.append([e, e_step, rnd_move, r, done, deepcopy(n_state)])
     # env.render()
     if total_reward == 5:
         print("All robots eliminated. Total reward =", total_reward)
@@ -103,4 +105,4 @@ while e < EPISODES:
         print("Agent eliminated. Total reward =", total_reward)
     e += 1
 
-# write_chase_log(state_log, 'Random')
+write_chase_log(state_log, 'Random')
